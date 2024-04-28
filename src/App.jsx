cad9b3067d723546a5adbb293e8983ff09af91f8
Jonathan Silva <jonathan.silva@nazar.cl>
2024-04-28 19:54:03 -0400

    ItemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Navbar from './Components/NavBar';
 import ItemListContainer from './Components/ItemListContainer';
 import ItemDetail from './Components/ItemDetail';
 import Cart from './pages/Cart';
