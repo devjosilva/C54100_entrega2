e059cd0186d45472a4bb0c773b76802d17f04d33
Jonathan Silva <jonathan.silva@nazar.cl>
2024-04-28 23:33:12 -0400

    Modifica vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/entrega2/",
+  base: "./",
   server: { //Agregue esta configuracion por error en App.jsx 
     fs: {
       cachedChecks: false
     }
-  }
+  },
+  build: {
+    outDir: 'dist', // Define la carpeta de salida como 'dist'
+    assetsDir: '.', // Asigna el directorio de activos a la raíz del proyecto
+    emptyOutDir: true, // Limpia la carpeta de salida antes de construir
+  },
 })
