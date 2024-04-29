7292c878373341fa480a5354fbf638dc7824900d
Jonathan Silva <jonathan.silva@nazar.cl>
2024-04-28 21:39:54 -0400

    Configurar githubpage

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/entrega2/",
   server: { //Agregue esta configuracion por error en App.jsx 
     fs: {
       cachedChecks: false
