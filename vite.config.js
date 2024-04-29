53c2aba4acf1991f372bcd35002748a480e72d7b
Jonathan Silva <jonathan.silva@nazar.cl>
2024-04-28 20:24:44 -0400

    Añade configuracion vite.config.js error ItemListContainer

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { //Agregue esta configuracion por error en App.jsx 
+    fs: {
+      cachedChecks: false
+    }
+  }
 })
